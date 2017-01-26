a56d2d5179b5e2a5b0cd021c85e606eff0b21c45
Andrea Scian <andrea.scian@dave.eu>
2017-01-26 12:08:58 +0100

    tools: add speedtest-cli stub
    I have no time now to write the whole script, but at least put a
    reference for it!

---
+++ tools/speedtest-cli.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# TODO:
+# * check if speedtest-cli exists
+# * if not check if we have a temporary copy in /tmp
+# * if not, download a temporary copy from https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+# * execute it with needed params
+
+echo "get your copy with 'wget -O speedtest https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py'"
